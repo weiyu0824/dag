a0fbbc934828c3ce5aec3ccd5ef67564d8f36291
wylin2 <wylin2@node0.query-exp.gaea-pg0.wisc.cloudlab.us>
2024-03-14 02:40:01 -0500

    fix bug add multi-object detection

--- experiments/query/exp1.py
+++ experiments/query/exp1.py
@@ -15,6 +15,32 @@ def draw(img_path, bboxes, save_path):
         cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, thickness) 
     cv2.imwrite(save_path, img)
 
+# class matcher
+num_classes = 2
+NuimageCategoryToId = {}
+CocoCategoryToId = {}
+person_id = 0
+vehicle_id = 1
+NuimageCategoryToId['human.pedestrian.adult'] = person_id
+NuimageCategoryToId['human.pedestrian.child'] = person_id
+NuimageCategoryToId['human.pedestrian.construction_worker'] = person_id
+NuimageCategoryToId['human.pedestrian.personal_mobility'] = person_id
+NuimageCategoryToId['human.pedestrian.police_officer'] = person_id
+NuimageCategoryToId['human.pedestrian.stroller'] = person_id
+NuimageCategoryToId['human.pedestrian.wheelchair'] = person_id
+NuimageCategoryToId['vehicle.bicycle'] = vehicle_id
+NuimageCategoryToId['vehicle.car'] = vehicle_id
+NuimageCategoryToId['vehicle.motorcycle'] = vehicle_id 
+NuimageCategoryToId['vehicle.truck'] = vehicle_id 
+NuimageCategoryToId['vehicle.bus.rigid'] = vehicle_id
+NuimageCategoryToId['vehicle.bus.bendy'] = vehicle_id
+NuimageCategoryToId['vehicle.trailer'] = vehicle_id
+CocoCategoryToId[0] = person_id
+CocoCategoryToId[1] = vehicle_id
+CocoCategoryToId[2] = vehicle_id
+CocoCategoryToId[3] = vehicle_id
+CocoCategoryToId[5] = vehicle_id
+CocoCategoryToId[7] = vehicle_id
 
 
 # Load data
@@ -22,149 +48,147 @@ root = 'nuscenes-devkit/data/sets/nuimages/'
 nuim = NuImages(dataroot=root, version='v1.0-mini', verbose=False, lazy=True)
 num_frame = len(nuim.sample)
 
-# Load model
-model_name = 'yolov8l'
-model = YOLO(f'{model_name}.pt')
-model.info()
+def measure(model_name, iou_theshold):
 
 
-# metric
-pre_latency, inf_latency, post_latency = [], [], []
-for i in tqdm.tqdm(range(num_frame)):
-    sample = nuim.sample[i]
-    key_camera_token = sample['key_camera_token']
+    # Load model
+    # model_name =
+    #  'yolov8m'
+    model = YOLO(f'{model_name}.pt')
+    model.info()
 
-    # Get object annos & category
-    bboxes, categories = [], []
-    for obj_ann in nuim.object_ann:
-        if obj_ann['sample_data_token'] == 'key_camera_token':
-            bboxes.append(obj_ann['bbox'])
-            categories.append(nuim.get('category', obj_ann['category_token']))
+    def get_ground_truth_box(sample):
+        if not sample:
+            raise ValueError('sample should not be none')
+        key_camera_token = sample['key_camera_token']
+        # object_tokens, surface_tokens = nuim.list_anns(sample['token'])
+        bboxes = [o['bbox'] for o in nuim.object_ann if o['sample_data_token'] == key_camera_token]
+        categories = [nuim.get('category', o['category_token'])['name']
+                    for o in nuim.object_ann if o['sample_data_token'] == key_camera_token]
+        # print(categories)
+        # print(bboxes)    
 
-    # Get filename
-    filename = nuim.get('sample_data', sample['key_camera_token'])['filename']
-
-    # inference
-    import time 
-    st = time.time()
-    # print(st)
-    results = model.predict(root + filename, save=False)
-    result = results[0]
-    # pred_bboxes = results[0].boxes.xyxyn.numpy()   
-    # print(results)
-    # print(time.time() - st)
-
-    # calculate latency 
-    pre_latency.append(result.speed['preprocess'])
-    inf_latency.append(result.speed['inference'])
-    post_latency.append(result.speed['postprocess'])
-
-    # print(result.conf)
-    pred_clss = result.boxes.cls
-    pred_confs = result.boxes.conf
-    pred_bboxes = result.boxes.xyxy.round().int()
-    
-    # pred_boxes = 
-    attr = torch.stack([pred_clss, pred_confs]).T
-    pred_boxes = torch.cat([pred_bboxes, attr], dim=1 )
-    print(pred_boxes)
-
-    # gt boxes=
-     
-
-    # calculate mAP
-    # calculate_map(true_boxes=, pred_boxes=)
-
-
-print(sum(pre_latency)/num_frame)
-print(sum(inf_latency)/num_frame)
-print(sum(post_latency)/num_frame)
-exit()
-
-key_camera_token = sample['key_camera_token']
-# object_tokens, surface_tokens = nuim.list_anns(sample['token'])
-bboxes = [o['category_token'] for o in nuim.object_ann if o['sample_data_token'] == key_camera_token]
-print(bboxes)
-print(nuim.get('category', bboxes[0]))
-exit()
-
-img_path = root + nuim.get('sample_data', sample['key_camera_token'])['filename']
-
-# draw(img_path, bboxes, 'test-1.jpg')
-# img_path = '/Users/andersonlin/Documents/research/chain-optimize/nuscenes-devkit/data/sets/nuimages/samples/CAM_FRONT_RIGHT/n013-2018-08-27-14-41-26+0800__CAM_FRONT_RIGHT__1535352274870176.jpg'
-
-# # key_camera_token = sample['key_camera_token']
-# # nuim.render_image(key_camera_token, object_tokens=object_tokens)
-
-from ultralytics import YOLO 
-print('Load')
-model_name = 'yolov8x'
-model = YOLO(f'{model_name}.pt')
-print('Info')
-model.info()
-# print('Train')
-# results = model.train(data='coco8.yaml', epochs=100, imgsz=640)
-print('Inf')
-results = model.predict(img_path, save=False)
-print(results[0].boxes)
-
-exit()
-org_h, org_w = results[0].orig_shape
-# print(results[0].boxes.xywh.numpy())
-# print(results[0].boxes.xyxy.numpy())
-print(results[0].boxes.xyxy.numpy())
-print(results[0].boxes.xyxyn.numpy())
-
-bboxes = results[0].boxes.xyxyn.numpy()
-
-bboxes[:, 0] = bboxes[:, 0] * org_w
-bboxes[:, 1] = bboxes[:, 1] * org_h
-bboxes[:, 2] = bboxes[:, 2] * org_w
-bboxes[:, 3] = bboxes[:, 3] * org_h
-
-# # print(results)
-
-# bboxes = results[0].boxes.xywh.numpy()
-# bboxes[:, 2] = bboxes[:, 0] + bboxes[:, 2] 
-# bboxes[:, 3] = bboxes[:, 1] + bboxes[:, 3]
-bboxes = np.round(bboxes).astype(np.int)
-print(bboxes)
-
-# draw(img_path, bboxes, f'pred-{model_name}.jpg')
-
-
-# import yolov5
-# for model_name in ['keremberke/yolov5n-aerial-sheep', 'keremberke/yolov5s-aerial-sheep', 'keremberke/yolov5m-aerial-sheep']:
-#     model = yolov5.load(model_name)
-#     print('Loaded')
-#     model.conf = 0.25  # NMS confidence threshold
-#     model.iou = 0.45  # NMS IoU threshold
-#     model.agnostic = False  # NMS class-agnostic
-#     model.multi_label = False  # NMS multiple labels per box
-#     model.max_det = 1000  # maximum number of detections per image
-#     print('Perform')
-#     # perform inference
-#     preprocess_times, inference_times, postprocess_times = [], [], []
-#     num_frame = 1
-#     for sample_idx in tqdm.tqdm(range(num_frame)):
-#         img_path = root + nuim.sample_data[sample_idx]['filename']
-#         print(img_path)
-#         results = model(img_path)
-#         preprocess_times.append(results.times[0].t)
-#         inference_times.append(results.times[1].t)
-#         postprocess_times.append(results.times[2].t)
-
-#         # parse results
-#         predictions = results.pred[0]
-#         boxes = predictions[:, :4] # x1, y1, x2, y2
-#         scores = predictions[:, 4]
-#         categories = predictions[:, 5]
-#         print(boxes, scores, categories)
+        # num_boxes = len(bboxes)
+        # categories = torch.zeros(size=(len(categories), 1))
+        # bboxes = torch.tensor(bboxes)
+        # print('gt' , categories)
+        filtered_categories, filtered_bboxes = [], []
+        # true_boxes = []
+        for category, bbox in zip(categories, bboxes):
+            # print(type(category), category)
+            if category not in NuimageCategoryToId:
+                # print('filter: ', category)
+                continue
+            filtered_categories.append(NuimageCategoryToId[category])
+            filtered_bboxes.append(bbox)
+            # true_boxes.append(torch.cat([torch.tensor(bbox), torch.tensor(NuimageCategoryToId[category])]))
+        # print(true_boxes)
         
-#         # Calculate Acc
-#         # mAp:
+        filtered_bboxes = torch.tensor(filtered_bboxes)
+        filtered_categories = torch.unsqueeze(torch.tensor(filtered_categories), dim=1) 
+        true_boxes = torch.cat([filtered_bboxes, filtered_categories], dim=1) 
+        # print('true', true_boxes)
+        # exit(0)
+        return true_boxes 
 
-#     break
-#     print(f'Avg speed {model_name}: \
-#             pre-process: {sum(preprocess_times)/num_frame}, \
-#             inference: {sum(inference_times)/num_frame}')
+
+    # metric
+    pre_latency, inf_latency, post_latency = [], [], []
+    mean_APs = []
+    for i in range(num_frame):
+        sample = nuim.sample[i]
+        # key_camera_token = sample['key_camera_token']
+
+        # # Get object annos & category
+        # bboxes, categories = [], []
+        # for obj_ann in nuim.object_ann:
+        #     if obj_ann['sample_data_token'] == 'key_camera_token':
+        #         bboxes.append(obj_ann['bbox'])
+        #         categories.append(nuim.get('category', obj_ann['category_token']))
+
+        # Get filename
+        filename = nuim.get('sample_data', sample['key_camera_token'])['filename']
+
+        # inference
+        # import time 
+        # st = time.time()
+        # print(st)
+        results = model.predict(root + filename, save=False)
+        result = results[0]
+        # pred_bboxes = results[0].boxes.xyxyn.numpy()   
+        # print(results)
+        # print(time.time() - st)
+
+        # calculate latency 
+        pre_latency.append(result.speed['preprocess'])
+        inf_latency.append(result.speed['inference'])
+        post_latency.append(result.speed['postprocess'])
+
+        raw_pred_clss = result.boxes.cls 
+        raw_pred_confs = result.boxes.conf
+        raw_pred_bboxes = result.boxes.xyxy.round().int()
+
+        # print(pred_confs)
+        filtered_pred_bboxes, filtered_pred_clss, filtered_pred_confs = [], [], []
+        for pred_cls, pred_conf, pred_bbox in zip(raw_pred_clss, raw_pred_confs, raw_pred_bboxes):
+            
+            pred_cls = int(pred_cls.item())
+            # print(pred_cls)
+            # print(CocoCategoryToId)
+            if pred_cls not in CocoCategoryToId:
+                continue
+            filtered_pred_clss.append(CocoCategoryToId[pred_cls])
+            filtered_pred_confs.append(pred_conf)
+            filtered_pred_bboxes.append(pred_bbox)
+        
+        # print(filtered_pred_bboxes)
+        # print(filtered_pred_clss)
+        # print(filtered_pred_confs)
+        if len(filtered_pred_bboxes) == 0:
+            pred_boxes = []
+        else:
+            # print(filtered_pred_confs)
+            # print(filtered_pred_clss)
+            attr = torch.stack([torch.tensor(filtered_pred_clss), torch.tensor(filtered_pred_confs)]).T
+            # print(attr)
+            # print(filtered_pred_bboxes)
+            pred_boxes = torch.cat([torch.stack(filtered_pred_bboxes), attr], dim=1 )
+
+
+        # gt boxes=
+        true_boxes = get_ground_truth_box(sample)
+        # print(true_boxes)
+        # print(pred_boxes)
+        # exit(0)
+        # print(true_boxes)
+        # print('cur frame:', i, 'num gt', len(true_boxes), 'num de', len(pred_boxes))
+        mean_AP = calculate_map(true_boxes=true_boxes, pred_boxes=pred_boxes, iou_threshold=0.5, num_classes=num_classes)
+        # print('mAP: ', mean_AP.item())    
+        mean_APs.append(mean_AP.item()) 
+        # import pdb
+        # breakpoint()
+
+        # break
+        # print(mean_AP)
+        # exit()
+        # calculate mAP
+        # calculate_map(true_boxes=, pred_boxes=)
+    print(mean_APs)
+    return sum(mean_APs)/num_frame, sum(pre_latency)/num_frame, sum(inf_latency)/num_frame, sum(post_latency)/num_frame
+
+
+def main():
+    result = [] 
+    model_names = ['yolov8n','yolov8s', 'yolov8m', 'yolov8l', 'yolov8x'] 
+    # model_names = ['yolov8m']
+    iou_thresholds = [0.5]
+
+    for model_name in model_names:
+        for iou_threshold in iou_thresholds:
+            print(model_name, iou_threshold)
+            metric = measure(model_name, iou_threshold)
+            result.append([model_name, iou_threshold, metric])
+    print(result)
+    
+if __name__ == "__main__":
+    main()
--- experiments/query/metric.py
+++ experiments/query/metric.py
@@ -27,14 +27,14 @@ def calculate_iou(box1, box2):
     return iou
 
 
-def calculate_map(true_boxes, pred_boxes, iou_threshold=0.5, eps=10e-5):
+def calculate_map(true_boxes, pred_boxes, iou_threshold=0.5, num_classes=1, eps=10e-5):
     """Calculate mean average precision (mAP) for object detection.
         
     """
     # box_format = [x1, y1, x2, y2, cls, conf]
     average_precisions = []
 
-    num_classes = 1
+    # num_classes = 1
     for c in range(num_classes):
         detections = []
         ground_truths = []
@@ -52,18 +52,21 @@ def calculate_map(true_boxes, pred_boxes, iou_threshold=0.5, eps=10e-5):
         FP = torch.zeros(len(pred_boxes))
         total_true_bboxes = len(ground_truths)
         detected = [False for _ in range(len(ground_truths))] 
-
+        # breakpoint()
         for detection_idx, detection in enumerate(detections):
            # Find what gt box does this detection match
-            best_idx, best_iou = 0, 0
+            best_idx, best_iou = -1, 0
             for ground_truth_idx, ground_truth in enumerate(ground_truths):
                 iou = calculate_iou(ground_truth[:4], detection[:4])
                 if iou > best_iou:
                     best_iou = iou
                     best_idx = ground_truth_idx
             
+            # if best_idx == -1:
+            # breakpoint()
+            # print('bug:', len(ground_truths), c,  len(detections), best_idx)
             # Check if this detection is FP or TP
-            if detected[best_idx] == False and best_iou > iou_threshold:
+            if best_iou > iou_threshold and detected[best_idx] == False:
                TP[detection_idx] = 1
             else:
                FP[detection_idx] = 1
@@ -73,11 +76,12 @@ def calculate_map(true_boxes, pred_boxes, iou_threshold=0.5, eps=10e-5):
         TP_cumsum = torch.cumsum(TP, dim=0)
         FP_cumsum = torch.cumsum(FP, dim=0)
         precisions = TP_cumsum / (TP_cumsum + FP_cumsum + eps) 
-        recalls = TP_cumsum / total_true_bboxes
+        recalls = TP_cumsum / (total_true_bboxes + eps)
 
-        # ??
-        precisions = torch.cat(torch.tensor([1], precisions))
-        recalls = torch.cat(torch.tensor([0]), recalls)
+        # ?num_classes=1, )?
+        precisions = torch.cat([torch.tensor([1]), precisions])
+        recalls = torch.cat([torch.tensor([0]), recalls])
         average_precisions.append(torch.trapz(precisions, recalls)) 
-
+        # print('cls', c, precisions, recalls, TP_cumsum, FP_cumsum)
+    # print(average_precisions)
     return  sum(average_precisions) / len(average_precisions)
